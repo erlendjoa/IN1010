c91c51e28e398c8dae5200b9cee3defe2b38fd71
Erlend Joa <erlend.joa@gmail.com>
2023-03-28 18:40:32 +0200

    m

--- Trix/Traader/NteTall.java
+++ Trix/Traader/NteTall.java
@@ -1,19 +1,35 @@
 import java.util.*;
 import java.util.concurrent.*;
 
-// Trix
+// a = løst
+// b = ikke løst
+// c = løst
 
-public class NteTall extends Thread{
+public class NteTall implements Runnable{
+
+    int start;
+    int max;
+
+    public NteTall(int start, int max) {
+        this.start = start;
+        this.max = max;
+    }
 
     public static void main(String[] args) {
 
         NteTall ntetall = new NteTall();
-        ntetall.start();
+        Thread nyTraad1 = new Thread(ntetall);
+        nyTraad1.start();
+
+        for (int i = 0; i < 10; i++) {
+            Thread nyTraad3 = new Thread(ntetall);
+            nyTraad3.start();
+        }
     }
     
     @Override
     public void run() {
-        for (int i = 5; i <= 1000; i+=5) {
+        for (int i = 5; i <= 10000; i+=10) {
             System.out.println(i);
         }
     }

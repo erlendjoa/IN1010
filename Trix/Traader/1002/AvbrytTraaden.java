306eb2ec6e57a7c2225ccbc006068e7271ebfbd4
Erlend Joa <erlend.joa@gmail.com>
2023-03-29 01:23:12 +0200

    m

--- Trix/Traader/1002/AvbrytTraaden.java
+++ Trix/Traader/1002/AvbrytTraaden.java
@@ -2,9 +2,7 @@ public class AvbrytTraaden {
 
     public static void main(String[] args) {
         MyRunnable myRunnable = new MyRunnable();
-        Thread traad = new Thread(myRunnable) {
-            traad.start();
-            traad.interrupt();
-        }
+        Thread traad = new Thread(myRunnable);
+        traad.start();
     }
 }

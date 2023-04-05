acc38a04df98906d1081758d4f7e97aeaa0c4418
= <erlenjoa@uio.no>
2023-04-05 21:01:02 +0200

    m

---
+++ Obliger/Oblig_5/Subsekvens.java
@@ -0,0 +1,20 @@
+public class Subsekvens {
+
+    public final String subskv;
+    private int antall;
+
+    public Subsekvens(String subskv, int antall) {
+        this.subskv = subskv;
+        this.antall = antall;
+    }
+
+    public String hentSubsekvens(){
+        return subskv; }
+
+    public int hentAntall() {
+        return antall; }
+
+    public String toString() {
+        return "( " + subskv + "," + antall + ")";
+    }
+}

a6b3dde023871fad9e4051552cafc2b3af9122a9
Erlend Joa <erlend.joa@gmail.com>
2023-04-19 15:45:35 +0200

    m

--- Privat/Web/BlackJack/blackJack.js
+++ Privat/Web/BlackJack/blackJack.js
@@ -27,13 +27,12 @@ class Player {
     updateCards() {
         let tempScore = 0;
         let tempHand = [];
-        for (i = 0; i < this.hand.length; i++) {
-                this.ids[i].src = this.hand[i].getImage();
+        for (let i = 0; i < this.hand.length; i++) {
+                this.ids[i].src = this.hand[i].image;
                 this.ids[i].style.border = "3px solid black";
                 this.ids[i].style.display = "block";
                 tempHand.push(this.hand[i]);
                 tempScore += this.hand[i].value;
-                setTimeout(() => {}, 200); // Add delay of 200ms after displaying each card
         }
         this.hand = tempHand;
         this.score = tempScore;
@@ -47,49 +46,43 @@ class Player {
     }
 }
 
+
 // Get dealer and user Ids.
-dealerScoreId = document.getElementById("dealerScore");
-userScoreId = document.getElementById("playerScore");
-dealerIds = [];
-userIds = [];
-for (i = 0; i < 8; i++) {
+let dealerScoreId = document.getElementById("dealerScore");
+let userScoreId = document.getElementById("playerScore");
+let dealerIds = [];
+let userIds = [];
+for (let i = 0; i < 8; i++) {
     dealerIds.push(document.getElementById("cid" + i));
     userIds.push(document.getElementById("ciu" + i));
 }
 
 // Create and push new Cards to deck[].
-deck = [];  
-typeArr = ["club", "heart", "spade", "diamond"];
-valueArr = [11,2,3,4,5,6,7,8,9,10,10,10,10];
-for (i = 0; i < 4; i++) {
-    for (j = 1; j < 14; j++) {
+let deck = [];  
+let typeArr = ["club", "heart", "spade", "diamond"];
+let valueArr = [11,2,3,4,5,6,7,8,9,10,10,10,10];
+for (let i = 0; i < 4; i++) {
+    for (let j = 1; j < 14; j++) {
         deck.push(new Card("Assets/" + typeArr[i]+j + ".jpg", valueArr[j-1]));
     }
 }
 
+let user = new Player(userIds, userScoreId);
+let dealer = new Player(dealerIds, dealerScoreId);
 
-user = new Player(userIds, userScoreId);
-dealer = new Player(dealerIds, dealerScoreId);
+user.addCard(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
+user.addCard(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
+dealer.addCard(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
+dealer.addCard(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
 
-user.addCard(deck.pop(Math.floor(Math.random() * deck.length)));
-user.addCard(deck.pop(Math.floor(Math.random() * deck.length)));
-dealer.addCard(deck.pop(Math.floor(Math.random() * deck.length)));
-dealer.addCard(deck.pop(Math.floor(Math.random() * (deck.length-1 - 0 + 1))));
-
-// Hide dealers card.
+// Hide dealers card and score.
 dealerIds[0].src = "Assets/cardBack.jpg";
 dealerIds[1].src = "Assets/cardBack.jpg";
+document.getElementById("dealerScore").style.visibility = "hidden";
 
 
 function addCardFromButton() {
-    let randomIndex = 0;            // RANDOM NOT WORKING
-    let card = 0;
-
-    randomIndex = Math.floor(Math.random() * (deck.length-1 - 0 + 1));
-    card = deck.pop(Math.floor(Math.random() * (deck.length-1 - 0 + 1)));
-    
-    console.log(card);
-    user.addCard(card);
+    user.addCard(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
 }
 function lockInn() {
     document.getElementById("lockInn").style.visibility = "hidden";
@@ -97,13 +90,16 @@ function lockInn() {
     
     setTimeout(function() {
         dealerIds[0].src = dealer.hand[0].getImage();
+        document.getElementById("dealerScore").textContent = dealer.hand[0].value
+        document.getElementById("dealerScore").style.visibility = "visible";
     }, 200);
     setTimeout(function() {
         dealerIds[1].src = dealer.hand[1].getImage();
-    }, 400);
+        document.getElementById("dealerScore").textContent = dealer.hand[0].value + dealer.hand[1].value
+    }, 600);
     setTimeout(function() {
         dealerAddRest();
-    }, 600);
+    }, 1000);
 }
 
 function switchAce(player, card, index) {
@@ -153,7 +149,7 @@ function dealerAddRest() {
     }
 
     if (dealer.score < user.score || dealer.score > 21) {
-        for (i = 0; i < dealer.hand.length; i++) {
+        for (let i = 0; i < dealer.hand.length; i++) {
             if (dealer.hand[i].value == 11) {
                 switchAce(dealer, dealer.hand[i], i);
                 dealerAddRest();
@@ -168,5 +164,5 @@ function dealerAddRest() {
     }
 
     console.log("user wins");
-}
+} 
 

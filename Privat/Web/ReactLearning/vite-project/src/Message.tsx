8c9c458c0920683f084183e153367537fcbac8cf
Erlend Joa <erlend.joa@gmail.com>
2023-04-20 01:00:33 +0200

    m

---
+++ Privat/Web/ReactLearning/vite-project/src/Message.tsx
@@ -0,0 +1,8 @@
+
+
+function Message() {    // PascalCasing
+    const name = "Erlend";
+    return <h1>Hello {name}</h1>    // JSX: JavaScript XML
+}
+
+export default Message;
---
+++ Privat/Web/ReactLearning/vite-project/src/components/ListGroup.tsx
@@ -0,0 +1,27 @@
+
+
+function ListGroup() {
+
+    let items = [
+        "New York", "San Fransisco",
+        "Tokyo", "London"
+    ]
+
+    //items = [];
+
+    return (
+    // <> for returning Fragment (aka. mutliple return values).
+    <> 
+        <h1>  List </h1>
+        <ul className="list-group">
+
+            {items.length == 0 && <p>No Item Found</p>}
+
+            {items.map(item => <li className="list-group-item" key={item}> {item} </li>)}
+
+        </ul>
+    </>
+  );
+}
+
+export default ListGroup;

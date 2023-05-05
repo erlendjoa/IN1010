3cd9408ca6fa5884025ce35958c25cce6291db70
Erlend Joa <erlend.joa@gmail.com>
2023-05-05 23:36:20 +0200

    m

---
+++ Privat/GPTChallenges/Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        Palindrome.check(scn.nextLine(), 0);
+    }
+
+    public static void check(String answer, int i) {
+        if (answer.length() == 1) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            try {
+                if (!(answer.charAt(i) == answer.charAt(answer.length()-1-i))) {
+                    System.out.println("The string is not a palidnrome.");
+                } else {
+                    check(answer, i+1);
+                }
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("The string is a palindrome.");
+            }
+        }
+    }
+}
